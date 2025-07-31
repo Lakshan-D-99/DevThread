0c241f64d6ef3624c0b0ba98c90d07f47a6a30d2
Dhanuka <lakshandhanuka398@gmail.com>
2025-07-31 23:13:47 +0200

    Added the Role as an Entity, because the users can be admins and normal users.

---
+++ src/main/java/com/sp/devthread/models/AppRole.java
@@ -0,0 +1,6 @@
+package com.sp.devthread.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/sp/devthread/models/Role.java
+++ src/main/java/com/sp/devthread/models/Role.java
@@ -1,4 +1,37 @@
 package com.sp.devthread.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Enumerated(EnumType.STRING)
+    private AppRole appRole;
+
+    public Role() {}
+
+    public Role(Long roleId, AppRole appRole) {
+        this.roleId = roleId;
+        this.appRole = appRole;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public AppRole getAppRole() {
+        return appRole;
+    }
+
+    public void setAppRole(AppRole appRole) {
+        this.appRole = appRole;
+    }
 }
