8cd6299a41b9062842a56d02347cb428c1c581fe
Dhanuka <lakshandhanuka398@gmail.com>
2025-07-31 23:20:37 +0200

    Updated AppUser class with the Attributes.

--- src/main/java/com/sp/devthread/models/AppUser.java
+++ src/main/java/com/sp/devthread/models/AppUser.java
@@ -1,3 +1,62 @@
 package com.sp.devthread.models;
 
-public class AppUser { }
+import jakarta.persistence.*;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String password;
+
+    public AppUser() {}
+
+    public AppUser(Long userId, String userName, String userEmail, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
