51b16c5a36288d49e18c7b4fb0c4e526e4118a8f
Dhanuka <lakshandhanuka398@gmail.com>
2025-07-31 23:46:29 +0200

    Implemented a Many to Many Relationship between AppUser and Role.

--- src/main/java/com/sp/devthread/models/AppUser.java
+++ src/main/java/com/sp/devthread/models/AppUser.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
@@ -26,4 +29,21 @@ public class AppUser {
 
     @Column(nullable = false)
     private String password;
+
+    /**
+     * A User can have multiple Roles, for an example a User can be an Admin or a just a User.Same goes with the Roles.
+     * One or Many Roles can be assigned to Many Users.
+     * Therfore we have a Many to Many Relationship between Users and Roles.
+     * User is the parent class.
+     */
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST,CascadeType.MERGE},
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
--- src/main/java/com/sp/devthread/models/Role.java
+++ src/main/java/com/sp/devthread/models/Role.java
@@ -15,6 +15,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
 
     @Enumerated(EnumType.STRING)
