630793469ea61d588d5f852debc3016d965a9b74
Dhanuka <lakshandhanuka398@gmail.com>
2025-08-01 09:29:32 +0200

    Added the Post Categories, because a Post can have multiple Categories.

---
+++ src/main/java/com/sp/devthread/models/PostCategory.java
@@ -0,0 +1,11 @@
+package com.sp.devthread.models;
+
+public enum PostCategory {
+    MOBILE_APP_DEVELOPMENT,
+    WEB_DEVELOPMENT,
+    NATIVE_APP_DEVELOPMENT,
+    HYBRID_APP_DEVELOPMENT,
+    JAVA,
+    JAVASCRIPT,
+    NODE_JS
+}
