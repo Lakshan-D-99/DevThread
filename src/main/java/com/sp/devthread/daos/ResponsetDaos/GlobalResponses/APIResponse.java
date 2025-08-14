51955e33f2b4b4b8cf51c31ce61a362038399493
Dhanuka <lakshandhanuka398@gmail.com>
2025-08-14 11:04:44 +0200

    APIResponse Class will act as a Messenger that will inform the client, if an API Request was Successful or not.

---
+++ src/main/java/com/sp/devthread/daos/ResponsetDaos/GlobalResponses/APIResponse.java
@@ -0,0 +1,14 @@
+package com.sp.devthread.daos.ResponsetDaos.GlobalResponses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private String message;
+}
