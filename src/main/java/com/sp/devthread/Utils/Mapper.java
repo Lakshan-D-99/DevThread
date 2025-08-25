63b5f9a2429e72b9c25d3bd8c4fdd3687c786278
Dhanuka <lakshandhanuka398@gmail.com>
2025-08-25 08:38:52 +0200

    Updated Mapper to convert Categories into DAOs.

--- src/main/java/com/sp/devthread/Utils/Mapper.java
+++ src/main/java/com/sp/devthread/Utils/Mapper.java
@@ -1,7 +1,11 @@
 package com.sp.devthread.Utils;
 
 import com.sp.devthread.daos.RequestDaos.PostRequests.PostRequestDao;
+import com.sp.devthread.daos.ResponsetDaos.CategoryResponses.CategoryResponseDao;
+import com.sp.devthread.daos.ResponsetDaos.CommentResponses.CommentResponseDao;
 import com.sp.devthread.daos.ResponsetDaos.PostResponses.PostResponseDao;
+import com.sp.devthread.models.Category;
+import com.sp.devthread.models.Comment;
 import com.sp.devthread.models.Post;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +25,25 @@ public class Mapper {
         postResponseDao.setCreatedDate(post.getCreatedDate());
         postResponseDao.setUserId(post.getUser().getId());
         return postResponseDao;
+    }
+
+    // Convert a Comment Entity into a CommentResponseDao
+    public CommentResponseDao conEntityToDao(Comment comment){
+        CommentResponseDao commentResponseDao = new CommentResponseDao();
+        commentResponseDao.setCommentId(comment.getId());
+        commentResponseDao.setCommentBody(comment.getCommentBody());
+        commentResponseDao.setPublishedDate(comment.getPublishedDate());
+        commentResponseDao.setPostId(comment.getPost().getId());
+        commentResponseDao.setUserId(comment.getUser().getId());
+        return commentResponseDao;
+    }
+
+    // Convert a Category Entity into a CategoryResponseDao
+    public CategoryResponseDao conEntityToDao(Category category){
+        CategoryResponseDao categoryResponseDao = new CategoryResponseDao();
+        categoryResponseDao.setCategoryId(category.getId());
+        categoryResponseDao.setCategoryName(category.getPostCategory().name());
+        return categoryResponseDao;
     }
 
 
