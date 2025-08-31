ce02c6b64ce422cfab748f55d4fcaeca10815166
Dhanuka <lakshandhanuka398@gmail.com>
2025-08-31 13:21:17 +0200

    Updated Mapper class to convert UserModels into UserDAOs

--- src/main/java/com/sp/devthread/Utils/Mapper.java
+++ src/main/java/com/sp/devthread/Utils/Mapper.java
@@ -1,12 +1,11 @@
 package com.sp.devthread.Utils;
 
-import com.sp.devthread.daos.RequestDaos.PostRequests.PostRequestDao;
 import com.sp.devthread.daos.ResponsetDaos.CategoryResponses.CategoryResponseDao;
 import com.sp.devthread.daos.ResponsetDaos.CommentResponses.CommentResponseDao;
 import com.sp.devthread.daos.ResponsetDaos.PostResponses.PostResponseDao;
-import com.sp.devthread.models.Category;
-import com.sp.devthread.models.Comment;
-import com.sp.devthread.models.Post;
+import com.sp.devthread.daos.ResponsetDaos.ProfileResponses.ProfileResponseDao;
+import com.sp.devthread.daos.ResponsetDaos.UserResponses.UserResponseDao;
+import com.sp.devthread.models.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -44,6 +43,28 @@ public class Mapper {
         categoryResponseDao.setCategoryId(category.getId());
         categoryResponseDao.setCategoryName(category.getPostCategory().name());
         return categoryResponseDao;
+    }
+
+    // Convert a Profile Entity into a ProfileResponseDao
+    public ProfileResponseDao conEntityToDao(Profile profile){
+        ProfileResponseDao pf = new ProfileResponseDao();
+        pf.setId(profile.getId());
+        pf.setFirstName(profile.getFirstName());
+        pf.setLastName(profile.getLastName());
+        pf.setBirthDate(profile.getBirthDate());
+        pf.setProfilePicture(profile.getProfilePicture());
+        pf.setBioData(profile.getBioData());
+        pf.setUserId(profile.getUser().getId());
+        return pf;
+    }
+
+    // Conver a User entitiy into UserResponseDao
+    public UserResponseDao conEntityToDao(User user){
+        UserResponseDao userResponseDao = new UserResponseDao();
+        userResponseDao.setId(user.getId());
+        userResponseDao.setUserName(user.getUserName());
+        userResponseDao.setUserEmail(user.getUserEmail());
+        return userResponseDao;
     }
 
 
